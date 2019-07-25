46658038c19cf384c9308d55127f40ada912f424
Mjr <mjr@informatix.io>
2019-07-25 11:45:32 -0400

    add fn

--- src/js/main.js
+++ src/js/main.js
@@ -4,11 +4,13 @@ class SourceAttribution {
 
     /**
      @class SourceAttribution
-     @summary TODO
+     @summary Copy/paste source attribution
      @param {Object} options - Supplied configuration
      @param {Object} [options.callbacks] - User supplied functions to execute at given stages of the component lifecycle
      @param {Function} options.callbacks.preCreate
      @param {Function} options.callbacks.postCreate
+     @param {Function} options.callbacks.preDestroy
+     @param {Function} options.callbacks.postDestroy
      */
 
     constructor(options) {
@@ -18,7 +20,9 @@ class SourceAttribution {
 
         const defaults = {};
 
-        defaults.TODO = null;
+        defaults.copyCategory = 'engagement';
+        defaults.copyEvent = 'copy';
+        defaults.productName = 'Product Name TBD';
 
         //put supplied options on top of defaults
         merge(this, defaults, options);
@@ -57,6 +61,34 @@ class SourceAttribution {
 
         if (this.callbacks !== undefined && this.callbacks[userFn] !== undefined && typeof this.callbacks[userFn] === 'function') {
             this.callbacks[userFn].apply(this, sliced);
+        }
+    }
+
+    /**
+     * @method trackEventGA
+     * @memberOf SourceAttribution
+     * @instance
+     * @summary standard function interface for different Google Analytics implementations
+     * @private
+     */
+    trackEventGA (category, action, label, value) {
+        if (value === undefined) {
+            value = 0;
+        }
+
+        if (typeof (window._gaq) !== 'undefined') {
+            window._gaq.push(['_trackEvent', category, action, label, value]);
+        }
+        else if (typeof (window.gtag) !== 'undefined') {
+            // jscs:disable
+            window.gtag('event', action, {
+                event_category: category, // jshint ignore:line
+                event_label: label // jshint ignore:line
+            });
+            // jscs:enable
+        }
+        else if (typeof (window.ga) !== 'undefined') {
+            window.ga('send', 'event', category, action, label, value);
         }
     }
 }
